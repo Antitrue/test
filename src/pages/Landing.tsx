c4e998a032dda10f3843144ae3ff73b2451f6171
LizaKarnomazova <karnomazova.liza@yandex.ru>
2024-08-31 22:32:50 +0400

    Form

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,11 @@
 import Greeting from '../widgets/landing/Greeting';
+import FormOnlineAppointment from '../widgets/landing/Form';
 
 function Landing() {
   return (
     <div>
       <Greeting />
+      <FormOnlineAppointment />
     </div>
   );
 }
