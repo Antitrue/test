2255221e08e16e91609925d5fdb0cf27ca282c99
k.revyakina <k.revyakina@kata.academy>
2024-09-03 13:21:57 +0300

    remove FormOnlineAppointment

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,10 @@
 import Greeting from '../widgets/landing/Greeting';
-import FormOnlineAppointment from '../widgets/landing/Form';
 import Description from '../widgets/landing/Description';
 
 function Landing() {
   return (
     <div>
       <Greeting />
-      <FormOnlineAppointment />
       <Description />
     </div>
   );
