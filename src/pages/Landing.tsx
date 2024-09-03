baab6fe96e5575883bd50fc938bedfe5e202296e
k.revyakina <k.revyakina@kata.academy>
2024-09-03 13:32:32 +0300

    add FormOnlineAppointment

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,13 @@
 import Greeting from '../widgets/landing/Greeting';
 import Description from '../widgets/landing/Description';
+import FormOnlineAppointment from '../widgets/landing/FormOnlineAppointment';
 
 function Landing() {
   return (
     <div>
       <Greeting />
       <Description />
+      <FormOnlineAppointment />
     </div>
   );
 }
