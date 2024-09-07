7440cd67d6486997021f2bc9238fca0db5e2a457
LizaKarnomazova <karnomazova.liza@yandex.ru>
2024-09-07 16:40:31 +0400

    clientNewsAPI

---
+++ src/shared/services/api/client/clientNews.api.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { baseUrl } from '../lib/consts';
+import { prepareHeaders } from '../lib/prepareHeadersApi';
+import { clientNewsResponse } from './clientNewsDTO';
+
+export const clientNewsAPI = createApi({
+  reducerPath: 'clientNewsAPI',
+  tagTypes: ['clientNews'],
+  baseQuery: fetchBaseQuery({ baseUrl: baseUrl, prepareHeaders }),
+  endpoints: build => ({
+    getClientNews: build.query<clientNewsResponse, void>({
+      query: () => ({
+        url: 'client/news',
+        method: 'GET',
+      }),
+      providesTags: ['clientNews'],
+    }),
+  }),
+});
+
+export const { useGetClientNewsQuery } = clientNewsAPI;
