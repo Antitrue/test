b1708b1675d4bf8691e22b1d4b949c68d692d10c
EmilKarabatyrov <emroyzkd@gmail.com>
2024-09-20 14:02:09 +0300

    Create vaccination.api.ts

--- src/shared/services/api/client/vaccination.api.ts
+++ src/shared/services/api/client/vaccination.api.ts
@@ -1,1 +1,57 @@
-console.log('s');
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { baseUrl, prepareHeaders } from '../lib';
+import { IAddProcedure, IUpdateProcedure, IVac } from './vaccinationDTO.ts';
+
+export const vaccinationApi = createApi({
+  reducerPath: 'vaccinationApi',
+  tagTypes: ['vaccination'],
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${baseUrl}/client/procedure`,
+    prepareHeaders,
+  }),
+  endpoints: build => ({
+    getAllVaccinations: build.query<IVac, number>({
+      query: id => ({
+        url: 'vaccination',
+        params: {
+          petId: id,
+        },
+      }),
+      providesTags: ['vaccination'],
+    }),
+    getVaccination: build.query<IVac, number>({
+      query: id => ({
+        url: `vaccination/${id}`,
+      }),
+      providesTags: ['vaccination'],
+    }),
+    addProcedure: build.mutation<IVac, IAddProcedure>({
+      query: ({ petId, body }) => ({
+        url: 'vaccination',
+        method: 'POST',
+        params: {
+          petId: petId,
+        },
+        body,
+      }),
+      invalidatesTags: ['vaccination'],
+    }),
+    updateProcedure: build.mutation<IVac, IUpdateProcedure>({
+      query: ({ id, body }) => ({
+        url: `vaccination/${id}`,
+        method: 'PUT',
+        body,
+      }),
+      invalidatesTags: ['vaccination'],
+    }),
+    deleteProcedure: build.mutation<IVac, number>({
+      query: id => ({
+        url: `vaccination/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['vaccination'],
+    }),
+  }),
+});
+// const { useDeleteProcedureMutation, useAddProcedureMutation, useUpdateProcedureMutation, useGetVaccinationQuery, useGetAllVaccinationsQuery } =
+//   vaccinationApi;
