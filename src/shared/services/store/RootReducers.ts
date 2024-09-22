42ea2925c7ea96c3d599a3f20aa0393b4b207907
EmilKarabatyrov <emroyzkd@gmail.com>
2024-09-22 16:36:14 +0300

    Add petClient rootReducer

--- src/shared/services/store/RootReducers.ts
+++ src/shared/services/store/RootReducers.ts
@@ -5,6 +5,7 @@ import { clientReviewAPI } from '../api/client/clientReview.api';
 import { petContactAPI } from '../api/client/petContact.api';
 import { clientNewsAPI } from '../api/client/clientNews.api';
 import { topicApi } from '../api/user/topic.api';
+import { petClientAPI } from '../api/client/petClient.api';
 
 const rootReducers = combineReducers({
   authenticationSlice: authenticationSlice.reducer,
@@ -14,6 +15,7 @@ const rootReducers = combineReducers({
   [petContactAPI.reducerPath]: petContactAPI.reducer,
   [clientNewsAPI.reducerPath]: clientNewsAPI.reducer,
   [topicApi.reducerPath]: topicApi.reducer,
+  [petClientAPI.reducerPath]: petClientAPI.reducer,
 });
 
 export default rootReducers;
