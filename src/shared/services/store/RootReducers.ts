c86d5dc1198084f4b640b189900f9718f986b2a7
Elena Shumilova <shumilovalena2011@gmail.com>
2024-10-16 16:49:46 +0700

    42-doctor-api-2

--- src/shared/services/store/RootReducers.ts
+++ src/shared/services/store/RootReducers.ts
@@ -12,6 +12,7 @@ import { clientAPI } from '../api/client/client.api';
 import { petClientAPI } from '../api/client/petClient.api';
 import { vaccinationApi } from '../api/client/vaccination.api';
 import { clientAppointmentAPI } from '../api/client/clientAppointment.api.ts';
+import { doctorApi } from '../api/doctor/doctor.api.ts';
 
 const rootReducers = combineReducers({
   authenticationSlice: authenticationSlice.reducer,
@@ -28,6 +29,7 @@ const rootReducers = combineReducers({
   [petClientAPI.reducerPath]: petClientAPI.reducer,
   [vaccinationApi.reducerPath]: vaccinationApi.reducer,
   [clientAppointmentAPI.reducerPath]: clientAppointmentAPI.reducer,
+  [doctorApi.reducerPath]: doctorApi.reducer,
 });
 
 export default rootReducers;
