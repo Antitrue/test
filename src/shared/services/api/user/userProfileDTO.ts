dcd8412da0725dec64b95a1b90af6ec2d4f4f842
eakameneva <kate103422@gmail.com>
2024-10-17 11:18:24 +0200

    feat: add userProfileDTO

---
+++ src/shared/services/api/user/userProfileDTO.ts
@@ -0,0 +1,10 @@
+export interface IUserProfile {
+  avatarUrl: string;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  discordId: string;
+  telegramId: string;
+  discordNotify: boolean;
+  emailNotify: boolean;
+}
