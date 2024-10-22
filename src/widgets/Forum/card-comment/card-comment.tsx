f64df2bcf205e885d4c07fc0b307e36eca1e6b3c
Alex <alex@Ushakov.local>
2024-10-22 16:40:24 +0500

    alltopics v1.0

--- src/widgets/Forum/card-comment/card-comment.tsx
+++ src/widgets/Forum/card-comment/card-comment.tsx
@@ -5,7 +5,26 @@ import Likes from '../../Likes';
 
 import styles from './card-comment.module.scss';
 
-const CardComment: React.FC = () => {
+type CardCommentProps = {
+  topic: {
+    title: string;
+    content: string;
+    creationDate: string;
+    topicStarter: {
+      firstname: string;
+      lastname: string;
+    };
+    commentDtoList: {
+      likes: number;
+      dislike: number;
+    }[];
+  };
+}
+
+const CardComment: React.FC<CardCommentProps> = ({ topic }) => {
+  const { title, content, creationDate, topicStarter, commentDtoList } = topic;
+  const firstComment = commentDtoList?.[0];
+
   return (
     <div className={styles.container}>
       <div className={styles.author}>
@@ -13,21 +32,23 @@ const CardComment: React.FC = () => {
           <Avatar size={154} src={null} icon={<UserOutlined />} />
         </div>
         <div className={styles.nickname}>
-          <Link to='#'>Jack Cat</Link>
+          <Link to='#'>{`${topicStarter.firstname} ${topicStarter.lastname}`}</Link>
         </div>
         <div className={styles['triangle-left']}></div>
       </div>
       <div className={styles.content}>
-        <div className={styles.date}>13 сентября 2022</div>
+        <div className={styles.date}>{new Date(creationDate).toLocaleDateString()}</div>
         <hr className={styles.separator}></hr>
         <div className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus animi veritatis molestias pariatur officiis.
-          Cum illum unde suscipit enim tenetur quos laboriosam sint necessitatibus?
+          <strong>{title}</strong>
+          <p>{content}</p>
         </div>
-        <div className={styles.like}>
-          <Likes isLike={true} count={1} />
-          <Likes isLike={false} count={200} />
-        </div>
+        {firstComment && (
+          <div className={styles.like}>
+            <Likes isLike={true} count={firstComment.likes} />
+            <Likes isLike={false} count={firstComment.dislike} />
+          </div>
+        )}
       </div>
     </div>
   );
